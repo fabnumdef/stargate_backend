fb9bd24759ffbf1f368aa4a7d125b8c2989bbcaf
Gaël Demette <gael.demette+git@qonfucius.team>
2020-02-08 17:37:30 +0100

    Export metrics

--- src/index.js
+++ src/index.js
@@ -5,13 +5,23 @@ debug('logging with debug enabled!');
 
 require('dotenv').config();
 import Koa from 'koa';
+import http from 'http';
 import compression from 'koa-compress';
+import Prometheus from 'prom-client';
 import apolloServer from './apollo-server';
 import routes from './routes';
+import metricsMiddleware from './middlewares/metrics';
 
 const port = process.env.PORT || 3000;
 
+const metricsInterval = Prometheus.collectDefaultMetrics();
+
+process.on('SIGTERM', () => {
+    clearInterval(metricsInterval);
+});
+
 const app = new Koa();
+app.use(metricsMiddleware);
 app.use(compression());
 app.use(routes);
 
@@ -20,3 +30,10 @@ apolloServer.applyMiddleware({ app, path: '/api' });
 app.listen(port);
 
 debug(`GraphQL API running at http://localhost:${port}/api`);
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': Prometheus.register.contentType });
+    res.end(Prometheus.register.metrics());
+}).listen(process.env.PROMETHEUS_PORT || 9091, '0.0.0.0');
+
+debug(`Prometheus exporter running at http://localhost:${process.env.PROMETHEUS_PORT}`);
