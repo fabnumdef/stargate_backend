3870e5258062cef589a5ede01d42ed815058cd17
Frederic Moreau <noloxe.web@gmail.com>
2020-04-15 08:43:46 +0200

    Fix import nanoid in tests

--- tests/resolvers/jwt/login.test.js
+++ tests/resolvers/jwt/login.test.js
@@ -1,4 +1,4 @@
-import nanoid from 'nanoid';
+import { nanoid } from 'nanoid';
 import queryFactory, { gql } from '../../helpers/apollo-query';
 import { generateDummyUser, createDummyUser } from '../../models/user';
 
