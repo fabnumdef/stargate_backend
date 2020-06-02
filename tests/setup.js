99d1cb475aeeb773fe20e1af9ad361d5af835335
Gaël Demette <gael.demette+git@qonfucius.team>
2020-06-02 23:54:49 +0200

    Listen for variables in CI environment

--- tests/setup.js
+++ tests/setup.js
@@ -3,6 +3,8 @@ import config from '../src/services/config';
 import MongooseService from '../src/services/mongoose';
 
 beforeAll(async () => {
+  config.set('mail:transporter:host', '');
+  config.set('mail:transporter:auth', null);
   const DATABASE = `${config.get('mongodb')}-test`;
   await MongooseService(DATABASE, { retryWrites: false });
   await mongoose.connection.db.dropDatabase();
