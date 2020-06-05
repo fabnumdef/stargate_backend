4e219cba1036f88e9faeab4173486d1b7d753cb1
Frederic Moreau <noloxe.web@gmail.com>
2020-06-05 18:24:24 +0200

    Add roles list && new user in fixtures

--- fixtures/data/user.js
+++ fixtures/data/user.js
@@ -1,15 +1,25 @@
 import { nanoid } from 'nanoid';
-import { ROLE_ADMIN, ROLE_SUPERADMIN } from '../../src/models/rules';
+import {
+  ROLE_ADMIN,
+  ROLE_SUPERADMIN,
+  ROLE_UNIT_CORRESPONDENT,
+  ROLE_ACCESS_OFFICE,
+  ROLE_SCREENING,
+  ROLE_SECURITY_OFFICER,
+} from '../../src/models/rules';
 import { MIDDLE_EARTH, MORDOR } from './campus';
+import { DWARFS } from './unit';
 
 export default async ({ log }) => {
   const superAdminPassword = nanoid();
   const sauronPassword = nanoid();
   const bilboPassword = nanoid();
+  const gimliPassword = nanoid();
 
   log.info(`Super admin password will be ${superAdminPassword}`);
   log.info(`Sauron password will be ${sauronPassword}`);
   log.info(`Bilbo password will be ${bilboPassword}`);
+  log.info(`Gimli password will be ${gimliPassword}`);
 
   return [
     {
@@ -43,6 +53,21 @@ export default async ({ log }) => {
       password: bilboPassword,
       roles: [
         { role: ROLE_ADMIN, campuses: [MIDDLE_EARTH] },
+      ],
+    },
+    {
+      firstname: 'Gimli',
+      lastname: 'Beard',
+      email: {
+        original: 'gimli@localhost',
+      },
+      password: gimliPassword,
+      roles: [
+        { role: ROLE_ADMIN, units: [DWARFS], campuses: [MIDDLE_EARTH] },
+        { role: ROLE_SECURITY_OFFICER, units: [DWARFS], campuses: [MIDDLE_EARTH] },
+        { role: ROLE_UNIT_CORRESPONDENT, units: [DWARFS], campuses: [MIDDLE_EARTH] },
+        { role: ROLE_ACCESS_OFFICE, units: [DWARFS], campuses: [MIDDLE_EARTH] },
+        { role: ROLE_SCREENING, units: [DWARFS], campuses: [MIDDLE_EARTH] },
       ],
     },
   ];
