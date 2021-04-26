85df672ff0adba741c5ced3db5b3c76e4fc5975b
LC <laurent.cagnard@def.gouv.fr>
2021-04-26 10:38:36 +0200

    adding all columns in CSV export file

--- src/routes/export.js
+++ src/routes/export.js
@@ -71,20 +71,65 @@ router.get('/export/:export_token', async (ctx) => {
         options.fields.splice(options.fields.findIndex((a) => a.value === fieldToBeRemoved.value), 1);
 
         // insert fields that does not exist in Visitor
-        const fieldToBeAdded1 = { label: 'SERVICE', value: 'emptyField' };
-        const fieldToBeAdded2 = { label: 'PAYS DE NAISSANCE', value: 'emptyfield' };
-        const fieldToBeAdded3 = { label: 'LIEU(X) DE TRAVAIL', value: 'emptyfield' };
-        const fieldToBeAdded4 = { label: 'DATE CRIBLAGE', value: 'dateScreening' };
-        const fieldToBeAdded5 = { label: 'TYPE DE BADGE', value: 'typeBadge' };
-        const fieldToBeAdded6 = { label: 'NOM PERSONNE VISITEE', value: 'emptyfield' };
-        const fieldToBeAdded7 = { label: 'PRENOM PERSONNE VISITEE', value: 'emptyfield' };
-        options.fields.splice(options.fields.findIndex((a) => a.label === 'DATE DEBUT VALIDITE'), 0, fieldToBeAdded1);
-        options.fields.splice(options.fields.findIndex((a) => a.label === 'ORIGINE'), 0, fieldToBeAdded2);
-        options.fields.splice(options.fields.findIndex((a) => a.label === 'ORIGINE'), 0, fieldToBeAdded3);
-        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded4);
-        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded5);
-        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded6);
-        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded7);
+        const fieldToBeAdded1 = { label: 'NOM ET EXTENSION PHOTO', value: 'emptyField' };
+        const fieldToBeAdded2 = {
+          label: 'Adresse mail du responsable "Accès" unité/entreprise OBLIGATOIRE',
+          value: 'emptyField',
+        };
+        const fieldToBeAdded3 = { label: 'CIVILITE', value: 'emptyField' };
+        const fieldToBeAdded4 = { label: 'SERVICE', value: 'emptyField' };
+        const fieldToBeAdded5 = { label: 'VALIDITE ACCES', value: 'emptyField' };
+        const fieldToBeAdded6 = { label: 'PAYS DE NAISSANCE', value: 'emptyfield' };
+        const fieldToBeAdded7 = { label: 'LIEU(X) DE TRAVAIL', value: 'emptyfield' };
+        const fieldToBeAdded8 = { label: 'DATE CRIBLAGE', value: 'dateScreening' };
+        const fieldToBeAdded9 = { label: 'ACCES HOMET', value: 'emptyfield' };
+        const fieldToBeAdded10 = { label: 'ACCES CACHIN', value: 'emptyfield' };
+        const fieldToBeAdded11 = { label: 'ACCES COMNORD', value: 'emptyfield' };
+        const fieldToBeAdded12 = { label: 'ACCES NARDOUET', value: 'emptyfield' };
+        const fieldToBeAdded13 = { label: 'ACCES ILOT SUD', value: 'emptyfield' };
+        const fieldToBeAdded14 = { label: 'ACCES H3', value: 'emptyfield' };
+        const fieldToBeAdded15 = { label: 'ADRESSE MAIL UNITE CONTRACTANTE', value: 'emptyfield' };
+        const fieldToBeAdded16 = { label: 'DROIT DE CONDUITE', value: 'emptyfield' };
+        const fieldToBeAdded17 = { label: 'PROFIL D\'ACCES', value: 'emptyfield' };
+        const fieldToBeAdded18 = { label: 'COMMENTAIRE', value: 'emptyfield' };
+        const fieldToBeAdded19 = { label: 'FREQUENCE DE VISITE', value: 'emptyfield' };
+        const fieldToBeAdded20 = { label: 'ENVOI MAIL', value: 'emptyfield' };
+        const fieldToBeAdded21 = { label: 'IMPORTATION PROTECTER', value: 'emptyfield' };
+        const fieldToBeAdded22 = { label: 'REFERENCE HABILITATION OU C.E.', value: 'emptyfield' };
+        const fieldToBeAdded23 = { label: 'DATE FIN DE VALIDITE HABILITATION OU C.E.', value: 'emptyfield' };
+        const fieldToBeAdded24 = { label: 'EXTENSION PHOTO', value: 'emptyfield' };
+        const fieldToBeAdded25 = { label: 'TYPE DE BADGE', value: 'typeBadge' };
+        const fieldToBeAdded26 = { label: 'NOM PERSONNE VISITEE', value: 'emptyfield' };
+        const fieldToBeAdded27 = { label: 'PRENOM PERSONNE VISITEE', value: 'emptyfield' };
+        const fieldToBeAdded28 = { label: 'CODE ACTION', value: 'emptyfield' };
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'NOM DE NAISSANCE'), 0, fieldToBeAdded1);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'NOM DE NAISSANCE'), 0, fieldToBeAdded2);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'NOM DE NAISSANCE'), 0, fieldToBeAdded3);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'DATE DEBUT VALIDITE'), 0, fieldToBeAdded4);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'DATE DEBUT VALIDITE'), 0, fieldToBeAdded5);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'ORIGINE'), 0, fieldToBeAdded6);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'ORIGINE'), 0, fieldToBeAdded7);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded8);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded9);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded10);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded11);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded12);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded13);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded14);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded15);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded16);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded17);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded18);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded19);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded20);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded21);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded22);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded23);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded24);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded25);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded26);
+        options.fields.splice(options.fields.findIndex((a) => a.label === 'N° DEMANDE'), 0, fieldToBeAdded27);
+        options.fields.push(fieldToBeAdded28);
 
         const parser = new Json2csv.Parser({
           transforms: [flatten()],
