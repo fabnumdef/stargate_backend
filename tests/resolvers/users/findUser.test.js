eea965143c9910e49a8035e182ab694e090928c0
Frederic Moreau <noloxe.web@gmail.com>
2021-05-26 15:52:52 +0200

    fix: test

--- tests/resolvers/users/findUser.test.js
+++ tests/resolvers/users/findUser.test.js
@@ -8,10 +8,12 @@ function mutateFindUser(email) {
     mutation: gql`
         mutation findUser($email: EmailAddress!) {
           findUser(email: $email) {
-            _id
+            id
             firstname
             lastname
-            email
+            email {
+                original
+            }
           }
         }
     `,
@@ -23,15 +25,15 @@ it('Find a user by email', async () => {
   const dummyUser = await createDummyUser();
 
   try {
-    const { findUserByEmail } = await mutateFindUser(dummyUser.email.canonical);
+    const { data: { findUser } } = await mutateFindUser(dummyUser.email.original);
 
     // const findUserByEmail = await User.findOne({ 'email.canonical': dummyUser.email.canonical });
 
-    expect(findUserByEmail._id).toStrictEqual(dummyUser._id);
+    expect(findUser.id).toStrictEqual(dummyUser._id.toString());
 
     const dbVersion = await User.findOne({ _id: dummyUser._id });
 
-    expect(dbVersion.email.canonical).toBe(dummyUser.email.canonical);
+    expect(dbVersion.email.original).toBe(dummyUser.email.original);
   } finally {
     await User.findOneAndDelete({ _id: dummyUser._id });
   }
